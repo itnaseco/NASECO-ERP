19ef3e32c43871d97a81525a7708a14b8ce88180
Matovu Matthew <itnaseco@gmail.com>
2026-01-07 15:56:25 +0300

    first commit

---
+++ naseco_erp/naseco_erp/doctype/farm_location/farm_location.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Matovu Matthew and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Farm Location", {
+// 	refresh(frm) {
+
+// 	},
+// });
